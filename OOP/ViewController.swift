8e5a0e5aa4083b394feb2799526bad34cdfbee23
Evan Christian <evanchristian98@gmail.com>
2019-05-15 08:16:11 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Evan Christian on 15/05/19.
+//  Copyright © 2019 Evan Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
